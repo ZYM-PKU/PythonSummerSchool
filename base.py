0e4f931ccf3f2fa1dfd5265bab2906e267613cfa
ZYM-PKU <82540602@qq.com>
2020-07-05 17:34:34 +0800

    update

--- base.py
+++ base.py
@@ -27,11 +27,42 @@ player.ay=2
 #跳跃
 player.jumptime=0#连续跳跃次数
 player.onbottom=True#是否在地上
-
+#死亡
+player.death=False
 
 #尖刺
-spine=Actor('spine')
-spine.bottomleft=(70,720)
+spines=[]
+spine=Actor('spine_up')
+spine.bottomleft=(500,BOTTOM)
+spine.points=[]
+spines.append(spine)
+spine=Actor('spine_down')
+spine.bottomleft=(250,BOTTOM)
+spine.points=[]
+spines.append(spine)
+
+def init():
+    for spine in spines:
+        if spine.image=='spine_up':
+            x,y=spine.bottomleft
+            for i in range(100):
+                x+=31/100
+                y-=61/100
+                spine.points.append((x,y))
+            for i in range(100):
+                x+=31/100
+                y+=61/100
+                spine.points.append((x,y))
+        else:
+            x,y=spine.topleft
+            for i in range(100):
+                x+=31/100
+                y+=61/100
+                spine.points.append((x,y))
+            for i in range(100):
+                x+=31/100
+                y-=61/100
+                spine.points.append((x,y))
 
 
 
@@ -41,6 +72,7 @@ def draw():
     screen.blit('cover',(0,0))
     bottom.draw()
     save.draw()
+    for spine in spines:spine.draw()
     player.draw()
     
 def update():
@@ -60,14 +92,23 @@ def update():
     if player.right>WIDTH:
         player.right=WIDTH
         player.vx=0
+    
+    #碰撞检测
+    for spine in spines:
+        for point in spine.points:
+            if player.collidepoint(point):
+                player.death=True
+    if player.death:
+        player.image='player_left_dead' if player.image=='player_left' else 'player_right_dead'
+        clock.schedule(reset,0.1)
 
 def on_key_down(key):
     #运动控制
     if key==key.RIGHT:
-        player.vx=10
+        player.vx=8
         player.image='player_right'
     if key==key.LEFT:
-        player.vx=-10
+        player.vx=-8
         player.image='player_left'
     if key==key.SPACE:
         if player.jumptime==2:
@@ -89,5 +130,15 @@ def on_key_up(key):
     if key==key.LEFT:
         if not player.image=='player_right':player.vx=0
 
+def reset():
+    player.image='player_right'
+    player.bottomleft=(0,BOTTOM)
+    player.vx=0
+    player.vy=0
+    player.jumptime=0#连续跳跃次数
+    player.onbottom=True#是否在地上
+    player.death=False
 
+
+init()
 pgzrun.go()
